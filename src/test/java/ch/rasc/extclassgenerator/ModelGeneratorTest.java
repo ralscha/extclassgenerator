1826b1fde183d4975ad49e3dcfde11ad9b256862
Ralph Schaer <ralphschaer@gmail.com>
2014-05-31 19:24:42 +0200

    Remove unused import statement

--- src/test/java/ch/rasc/extclassgenerator/ModelGeneratorTest.java
+++ src/test/java/ch/rasc/extclassgenerator/ModelGeneratorTest.java
@@ -19,7 +19,6 @@ import static org.fest.assertions.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
