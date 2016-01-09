6d26be0aea9591d9931fe2bd326563e4d868f238
Ralph Schaer <ralphschaer@gmail.com>
2016-01-09 07:06:23 +0100

    Upgrade libraries Update year in license header

--- src/main/java/ch/rasc/extclassgenerator/association/BelongsToAssociation.java
+++ src/main/java/ch/rasc/extclassgenerator/association/BelongsToAssociation.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013-2015 Ralph Schaer <ralphschaer@gmail.com>
+ * Copyright 2013-2016 Ralph Schaer <ralphschaer@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
